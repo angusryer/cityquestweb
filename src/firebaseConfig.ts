897bfeb15c0e785b6d720426f222333e19ddba54
Angus Ryer <angus@enabled.company>
2021-03-06 22:44:04 -0500

    modified firebase imports to prod mode

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -1,7 +1,4 @@
 import firebase from "firebase/app";
-import 'firebase/auth';
-import 'firebase/storage';
-import 'firebase/firestore';
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
